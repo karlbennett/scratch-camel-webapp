5ec24ba8d7471723abac9543358589c61c775ec0
Karl Bennett <karlbennett@orcon.net.nz>
2013-07-27 22:06:18 +1200

    Added a comment to the ScratchCamelServlet class.

--- src/main/java/scratch/camel/webapp/ScratchCamelServlet.java
+++ src/main/java/scratch/camel/webapp/ScratchCamelServlet.java
@@ -5,6 +5,8 @@ import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import javax.servlet.annotation.WebServlet;
 
 /**
+ * The Camel servlet that will accept all the RESTful requests.
+ *
  * @author Karl Bennett
  */
 @WebServlet("/scratch/*") // Register the Camel servlet.
