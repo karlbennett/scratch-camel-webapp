f5bfe8932b42e74554f8620e9fd212515544fa29
Karl Bennett <karlbennett@orcon.net.nz>
2013-07-27 22:20:06 +1200

    - Added some comments to the ScratchCamelListener class.  - Registered the ScratchRouteBuilder class.

--- src/main/java/scratch/camel/webapp/ScratchCamelListener.java
+++ src/main/java/scratch/camel/webapp/ScratchCamelListener.java
@@ -2,16 +2,21 @@ package scratch.camel.webapp;
 
 import org.apache.camel.component.servletlistener.JndiCamelServletContextListener;
 
+import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
 
 /**
  * @author Karl Bennett
  */
-@WebListener
+@WebListener // Register the Camel context listener.
 public class ScratchCamelListener extends JndiCamelServletContextListener {
 
-    public ScratchCamelListener() {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
 
-        System.out.println("LISTENER STARTORED!");
+        // Register the Camel route builder that will configure the HTTP, JMS, and JPA endpoints.
+        sce.getServletContext().setInitParameter("routeBuilder-scratch", "scratch.camel.webapp.ScratchRouteBuilder");
+
+        super.contextInitialized(sce);
     }
 }
